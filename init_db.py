137f361720552efb43d21f0cb8fcce98bdf653e9
bolt162 <ash16299@gmail.com>
2025-09-28 17:28:54 -0700

    new commit

--- init_db.py
+++ init_db.py
@@ -1,7 +1,5 @@
 # init_db.py
-from hangman import db, create_app
-
-app = create_app()   # or import your app object - adjust if different
+from hangman import db, app
 
 with app.app_context():
     db.create_all()
